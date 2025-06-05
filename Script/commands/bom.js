09e2ae7a2b378313ad644745ac873e1b91eb6cdf
BULLET CYBER <124680876+bullet-cyber@users.noreply.github.com>
2025-06-05 20:35:31 +0600

    Create bom.js

---
+++ Script/commands/bom.js
@@ -0,0 +1,34 @@
+module.exports.config = {
+name: "bom",
+  version: "",
+  permssion: 2,
+  credits: "♕𝙼𝟿𝙷𝟺𝙼𝙼𝟺𝙳☞𝙱𝟺𝙳𝟿𝙻♕",
+  description: "",
+  category: "spam",
+  usages: "[msg] [amount]",
+  prefix: true,
+  cooldowns: 5,
+  dependencies: "",
+};
+
+module.exports.run = function ({ api, event, Users, args }) {
+  const permission = ["61568807452487"];
+   if (!permission.includes(event.senderID))
+   return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
+  if (args.length !== 2) {
+    api.sendMessage(`Invalid number of arguments. Usage: ${global.config.PREFIX}spam [msg] [amount]`, event.threadID);
+    return;
+  }
+  var { threadID, messageID } = event;
+  var k = function (k) { api.sendMessage(k, threadID)};
+
+  const msg = args[0];
+  const count = args[1];
+
+  //*vonglap
+
+for (i = 0; i < `${count}`; i++) {
+ k(`${msg}`);
+}
+
+}
